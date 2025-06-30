d85a3d2c74f2c1f061efaa5d45e9100bd0550743
Polo Valdivia <leovalper31@icloud.com>
2025-06-30 12:25:01 -0700

    actualizacion dos del segundo mundo

--- player.gd
+++ player.gd
@@ -45,7 +45,11 @@ func _on_reset_area_body_entered(body: Node2D) -> void:
 	
 
 
-
 func _on_caliz_body_entered(body: Node2D) -> void:
 	get_tree().change_scene_to_file("res://world_2.tscn")
+	pass # Replace with function body.
+
+
+func _on_reset_area_2_body_entered(body: Node2D) -> void:
+	get_tree().reload_current_scene()
 	pass # Replace with function body.
