ffed5d826720de597a7326ff6fef112977bea765
Uriel <urielaguilar936@gmail.com>
2025-06-30 12:52:27 -0600

    segundo nivel

--- player.gd
+++ player.gd
@@ -47,5 +47,5 @@ func _on_reset_area_body_entered(body: Node2D) -> void:
 
 
 func _on_caliz_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://world.tscn")
+	get_tree().change_scene_to_file("res://world_2.tscn")
 	pass # Replace with function body.
